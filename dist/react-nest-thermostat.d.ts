044b5a02f804178f5f1dac4bfb38b8b860b1b4a8
Christopher Aubut <christopher.aubut@charter.com>
2022-05-20 13:56:32 -0600

    bug(ts): mark all props optional

--- dist/react-nest-thermostat.d.ts
+++ dist/react-nest-thermostat.d.ts
@@ -2,16 +2,16 @@ declare module 'react-nest-thermostat' {
   import { FC } from 'react';
 
   type IThermostatPropTypes = {
-    height: string;
-    width: string;
-    numTicks: number;
-    minValue: number;
-    maxValue: number;
-    away: boolean;
-    leaf: boolean;
-    ambientTemperature: number;
-    targetTemperature: number;
-    hvacMode: 'cooling' | 'off' | 'heating';
+    height?: string;
+    width?: string;
+    numTicks?: number;
+    minValue?: number;
+    maxValue?: number;
+    away?: boolean;
+    leaf?: boolean;
+    ambientTemperature?: number;
+    targetTemperature?: number;
+    hvacMode?: 'cooling' | 'off' | 'heating';
   };
 
   const Thermostat: FC<IThermostatPropTypes>;
